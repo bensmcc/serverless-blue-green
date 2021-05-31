079bc3f4eb52932cde889a986344d41e2e154cc7
bensmcc <bensmcc@gmail.com>
2021-05-31 09:17:53 -0400

    Updates

--- handler.ts
+++ handler.ts
@@ -2,13 +2,14 @@ import { APIGatewayProxyHandler } from "aws-lambda"
 import { version } from './constants';
 
 export const hello: APIGatewayProxyHandler = async (event) => {
-    const possibleCodes = [200, 500];
+    const possibleCodes = [500];
     const randomCode = possibleCodes[Math.floor(Math.random() * possibleCodes.length)];
 
     return {
         statusCode: randomCode,
         body: JSON.stringify({
-            version: version
+            version: version,
+            code: randomCode
         })
     }
 }
